fe272759d12c45cb255b57d4d9f5b6ddf2b85fe2
Jayapriya-G <jayapriya9425@gmail.com>
2021-12-13 16:58:54 -0600

    moving second time to GIT

--- Testing/src/GitDemo/Sampletest.java
+++ Testing/src/GitDemo/Sampletest.java
@@ -1,9 +1,13 @@
 package GitDemo;
 
 public class Sampletest {
-
+static int a=4;
+static int b=5;
+static int c= a+b;
 	public static void main(String[] args) {
 	System.out.println("Hi code moving to git");
+	System.out.println("Hi second time moving code to git");
+	System.out.println(c);
 	}
 
 }
